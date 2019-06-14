40642f8d6a58554c8f55810df626ebb9eba778d2
oloruntobi <awoderuoloruntobi@gmail.com>
2019-06-14 08:50:50 +0100

    Used axios to get the list of smurfs I used axios get method to get the list of smurfs and displayed it to the screen using componentDidmount

--- village/src/App.js
+++ village/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import axios from "axios";
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
@@ -9,7 +9,14 @@ class App extends Component {
     super(props);
     this.state = {
       smurfs: [],
+      ErrorMessage: null
     };
+  }
+
+  componentDidMount(){
+   axios.get('http://localhost:3333/smurfs')
+   .then(res => this.setState({ smurfs: res.data }))
+   .catch(err => this.setState({ ErrorMessage: err.message }))
   }
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
